4412f6142e95029b9bbca17f404184befe2f9884
joopixel1 <joo@iastate.edu>
2023-09-30 15:17:22 -0500

    created structure

---
+++ Frontend/src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css'
+
+export default function Login() {
+
+   
+
+    return (
+        <div className='login'>
+            <h1>Login</h1>
+        </div>
+    )    
+};
---
+++ Frontend/src/Components/Signup/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Signup.css'
+
+export default function Signup() {
+
+   
+
+    return (
+        <div className='signup'>
+            <h1>Signup</h1>
+        </div>
+    )    
+};
