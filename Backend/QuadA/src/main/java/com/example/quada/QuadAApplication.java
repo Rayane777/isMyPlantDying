646b97e15ddee0b4bd326c4abdc16f34b45a1ac5
lvrd-ade <fadahunsiadeife11@gmail.com>
2023-09-30 15:56:06 -0500

    first_message

---
+++ Backend/QuadA/src/main/java/com/example/quada/QuadAApplication.java
@@ -0,0 +1,13 @@
+package com.example.quada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadAApplication.class, args);
+    }
+
+}
