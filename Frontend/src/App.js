297decdc43be5673dbc16e0f1a95349dae806ccd
joopixel1 <joo@iastate.edu>
2023-09-30 15:25:55 -0500

    str

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";// Bootstrap CSS
+import "bootstrap/dist/js/bootstrap.bundle.min";// Bootstrap Bundle JS
+
 import './App.css';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router >
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
+        </Routes>
+    </Router> 
     </div>
   );
 }
