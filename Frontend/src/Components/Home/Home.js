d3a00088be27b77eecc3f748fe30b0215e44310d
Rayane777 <84091947+Rayane777@users.noreply.github.com>
2023-10-01 04:51:47 +0000

    finished Homepage

--- Frontend/src/Components/Home/Home.js
+++ Frontend/src/Components/Home/Home.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Carousel from 'react-bootstrap/Carousel';
+import image1 from './image1.jpg';
+import image2 from './image2.jpg';
+import image3 from './image3.jpg';
+import image4 from './image4.jpg';
+import image5 from './image5.jpg';
+
 import './Home.css'
+
 
 export default function Home() {
 
@@ -7,7 +17,129 @@ export default function Home() {
 
     return (
         <div className='home'>
-            <h1>Home</h1>
+      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="/"> IsMyPlantDying</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/login">Log In</Nav.Link>
+          <Nav.Link href="/signup">Sign Up</Nav.Link>
+       
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    
+
+    <div>
+      <div>
+      <Carousel className="styled-div">
+        <Carousel.Item>
+          <img
+            className="carouselImg"
+            src={image1}
+            alt="First slide"
+          />
+          <div className="carousel-overlay">
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div><div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="carouselImg"
+            src={image2}
+            alt="Second slide"
+          />
+          <div className="carousel-overlay">
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div><div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="carouselImg"
+            src={image3}
+            alt="Third slide"
+          />
+          <div className="carousel-overlay">
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div><div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="carouselImg"
+            src={image4}
+            alt="Second slide"
+          />
+          <div className="carousel-overlay">
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+            <div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div><div className="text-overlay">
+              <h3>First Slide</h3>
+              <p>This is the first slide of the carousel.</p>
+            </div>
+          </div>
+        </Carousel.Item>
+        
+      </Carousel>
+      </div>
+    </div>
+        <div>
+          
+          <button>Start today!</button>
+          
         </div>
+        </div>
+        
     )    
 };
--- Frontend/src/Components/Login/Login.js
+++ Frontend/src/Components/Login/Login.js
@@ -1,13 +1,101 @@
-import React from 'react';
+
+import React, { useState } from "react";
+
+import navigate from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+
+import Button from "react-bootstrap/Button";
+
 import './Login.css'
+
 
 export default function Login() {
 
-   
+    const [email, setEmail] = useState("");
 
+    const [password, setPassword] = useState("");
+
+    const errors = {
+        uname: "invalid username",
+        pass: "invalid password"
+      };
+  
+    function validateForm() {
+  
+      return email.length > 0 && password.length > 0;
+  
+    }
+  
+    function handleSubmit(event) {
+  
+      event.preventDefault();
+  
+    }
+  
     return (
-        <div className='login'>
-            <h1>Login</h1>
-        </div>
-    )    
-};
+  
+      <div className="Login">
+
+        <div className="form">
+        <Form onSubmit={handleSubmit}>
+  
+  <Form.Group size="lg" controlId="email" className="input">
+
+    <Form.Label>Email</Form.Label>
+
+    <Form.Control
+
+      autoFocus
+
+      type="email"
+
+      value={email}
+
+      onChange={(e) => setEmail(e.target.value)}
+
+    />
+
+  </Form.Group>
+
+  <Form.Group size="lg" controlId="password" className="input">
+
+    <Form.Label>Password</Form.Label>
+
+    <Form.Control
+
+      type="password"
+
+      value={password}
+
+      onChange={(e) => setPassword(e.target.value)}
+
+    />
+
+  </Form.Group>
+
+  <Button block size="lg" type="submit" disabled={!validateForm()} className="mb-2">
+
+    Login
+
+  </Button>
+  
+  
+  
+</Form>
+<div className="newAccount">
+    <p>Don't have an account? <a href="./signup" >
+             Create an account
+        </a></p>
+
+</div>
+
+        </div >
+
+       
+
+      </div>
+  
+    );
+  
+  }
